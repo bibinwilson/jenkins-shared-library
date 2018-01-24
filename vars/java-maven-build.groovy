02f4efee8357e3cacc368598005829451ce3cfc0
Bibin Wilson <bibinwilsonn@gmail.com>
2018-01-24 10:39:37 -0800

    Update java-maven-build.groovy

--- vars/java-maven-build.groovy
+++ vars/java-maven-build.groovy
@@ -5,7 +5,7 @@ def call(body) {
         body.delegate = config
         body()
 
-        node(java) {
+        node(master) {
             // Clean workspace before doing anything
             deleteDir()
 
