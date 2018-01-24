34166ae53f2caa1fd6d098f6873067957a5a0606
Bibin Wilson <bibinwilsonn@gmail.com>
2018-01-24 12:21:13 -0800

    Update module.groovy

--- vars/module.groovy
+++ vars/module.groovy
@@ -1,6 +1,15 @@
-stage ('Clone') {
+def call(body) {
+
+        def config = [:]
+        body.resolveStrategy = Closure.DELEGATE_FIRST
+        body.delegate = config
+        body() 
+           
+        stage ('Clone') {
                    sh "echo 'Cloning ...'"
                 }
                 stage ('Build') {
                     sh "echo 'building...'"
                 }
+  
+}
